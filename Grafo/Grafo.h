d6055783bd17032d174cfb25d6d1e5e9d861a810
igorPhelype <igor.phelype@gmail.com>
2018-09-29 16:38:16 -0300

    Auto stash before merge of "master" and "origin/master"

---
+++ Grafo/Grafo.h
@@ -0,0 +1,20 @@
+//
+// Created by igorphelype on 29/09/18.
+//
+#include "Vertice.h";
+#include <list>
+#ifndef GRAFO_GRAFO_H
+#define GRAFO_GRAFO_H
+
+using namespace std;
+
+class Vertice;
+
+class Grafo {
+private:
+    list<Vertice> Vertices;
+
+};
+
+
+#endif //GRAFO_GRAFO_H
