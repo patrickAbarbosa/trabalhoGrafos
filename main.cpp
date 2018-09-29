6ea9927eefdf0bfac34315be3720fe4e89a865fd
igorPhelype <igor.phelype@gmail.com>
2018-09-29 19:28:31 -0300

    Auto stash before merge of "grafo" and "origin/vertice"

--- main.cpp
+++ main.cpp
@@ -4,13 +4,6 @@ using namespace std;
 
 int main()
 {
-    /*
-    list<int> l;
-    l.push_front(1);
-    // percorrer lista
-    for (std::list<int>::iterator it=l.begin() ; it != l.end(); ++it){
-        cout << *it;
-    }*/
     cout << "Hello world!" << endl;
     return 0;
 }
