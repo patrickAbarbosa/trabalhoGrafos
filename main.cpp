c3a3acdbe0e6ef5fad327193810e49330a37687d
igorPhelype <igor.phelype@gmail.com>
2018-09-30 18:54:05 -0300

    TIrando coisas inuteis da função main

--- main.cpp
+++ main.cpp
@@ -2,24 +2,14 @@
 #include <list>
 #include "Grafo.h"
 #include "Guloso.h"
-#include <chrono>
 
 using namespace std;
-//using namespace chrono;
 
 int main()
 {
-
-    __int64_t start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    Grafo a("grafoA.txt");
-
-    //__int64_t start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-
-
-    //Grafo a("grafo.txt");
+    Grafo a("grafo.txt");
     cout << "Tamanho da lista " << a.getTam() << endl;
     a.imprimir();
-
     Guloso guloso;
     list<int> solucao = guloso.calcular(a);
     cout << "Solucao" << endl;
@@ -27,8 +17,5 @@ int main()
     {
         cout << (*it) << ", ";
     }
-
-    //__int64_t end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    //cout << "It took" << start - end << "ms";
     return 0;
 }
