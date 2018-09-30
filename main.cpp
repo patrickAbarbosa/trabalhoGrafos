89b6fa7d13b22e7ebf713a71ce9608ab1db0a6a5
igorPhelype <igor.phelype@gmail.com>
2018-09-29 21:10:36 -0300

    adicionando timer

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
 #include <list>
 #include "Grafo.h"
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
 int main()
 {
-    /*
-    Grafo g;
-    g.adicionarVertice(1, 10);
-    g.adicionarVertice(2, 20);
-    g.adicionarVertice(3, 20);
-    g.adicionarAresta(1, 2, 100);
-    g.adicionarAresta(1, 3, 100);
-    g.adicionarAresta(3, 2, 100);
-    g.imprimir();
-    */
-
+    __int64_t start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
     Grafo a("grafo.txt");
     cout << "Tamanho da lista " << a.getTam() << endl;
     a.imprimir();
+    __int64_t end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    cout << "It took" << start - end << "ms";
     return 0;
 }
