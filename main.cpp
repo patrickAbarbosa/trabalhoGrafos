dd3308b68ee0e1a5db9b7e0c419cf35b5647b850
alexander <alexander.lmx@outlook.com>
2018-10-01 22:47:57 -0300

    Menu

--- main.cpp
+++ main.cpp
@@ -15,15 +15,74 @@ int main(int argc, char *argv[])
         arquivo = string(argv[1]);
 
     cout << "Carregando " << arquivo << "..." << endl;
-    Grafo a(arquivo);
-    cout << "Número de vértices: " << a.getTam() << endl;
-    //a.imprimir();
+    Grafo grafo(arquivo);
 
-    cout << "Buscando solução..." << endl;
     Guloso guloso;
     SolucaoGuloso solucao;
-    guloso.calcular(a, solucao);
-    cout << "Solução" << endl;
-    guloso.imprimir(solucao);
+
+    int aux;
+    int verticeA, verticeB;
+    float peso;
+
+    do
+    {
+        cout << "--------------- MENU ---------------"<< endl;
+        cout << "[0]  - Adicionar vertice." << endl;
+        cout << "[1]  - Remover vertice." << endl;
+        cout << "[2]  - Adicionar aresta." << endl;
+        cout << "[3]  - Remover aresta." << endl;
+        cout << "[4]  - Imprimir Grafo." << endl;
+        cout << "[5]  - Melhor solucao Gulosa" << endl;
+        cout << "[6]  - Numero de vértices" << endl;
+        cout << "[-1] - Encerrar" << endl;
+        cin >> aux;
+        cout << endl;
+        switch(aux)
+        {
+            case 0:
+                cout << "-- Adicionar Vertice" << endl;
+                cout << "Digite vertice e seu peso: ";
+                cin >> verticeA >> peso;
+                cout << endl;
+                grafo.adicionarVertice(verticeA, peso);
+                break;
+            case 1:
+                cout << "-- Remover Vertice" <<endl;
+                cout << "Digite o vertice: ";
+                cin >> verticeA;
+                grafo.removerVertice(verticeA);
+                break;
+            case 2:
+                cout << "-- Adicionar aresta ---" << endl;
+                cout << "Digite o vertice A, o vertice B e o peso da aresta ";
+                cin >> verticeA >> verticeB >>  peso;
+                grafo.adicionarAresta(verticeA, verticeB, peso);
+                break;
+            case 3:
+                cout << "-- Adicionar aresta ---" << endl;
+                cout << "Digite o vertice A e o vertice B";
+                cin >> verticeA >> verticeB;
+                grafo.removerAresta(verticeA, verticeB);
+                break;
+            case 4:
+                grafo.imprimir();
+                break;
+            case 5:
+                cout << "Buscando solução..." << endl;
+                guloso.calcular(grafo, solucao);
+                cout << "--- Solucao Gulosa ----" << endl;
+                guloso.imprimir(solucao);
+                break;
+            case 6:
+                cout <<"Quantidade de Vertices = " << grafo.getTam() << endl;
+                break;
+            case -1:
+                break;
+            default:
+                cout << "Opção inválida" << endl;
+                break;
+        }
+        cout << endl << endl;
+    }while(aux != -1);
     return 0;
 }
